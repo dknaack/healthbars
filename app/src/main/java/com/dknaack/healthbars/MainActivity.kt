55d84e54a03939ab2978196bd06e77027361264f
Daniel Knaack <daniel.knaack@protonmail.com>
2025-09-01 12:04:46 +0200

    Fix MainActivity

--- app/src/main/java/com/dknaack/healthbars/MainActivity.kt
+++ app/src/main/java/com/dknaack/healthbars/MainActivity.kt
@@ -6,7 +6,13 @@ import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.compose.animation.slideInVertically
 import androidx.compose.animation.slideOutVertically
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -19,15 +25,19 @@ import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.toRoute
 import androidx.room.Room
 import com.dknaack.healthbars.data.AppDatabase
-import com.dknaack.healthbars.data.HealthBar
 import com.dknaack.healthbars.ui.screens.list.ListScreen
 import com.dknaack.healthbars.ui.screens.list.ListViewModel
-import com.dknaack.healthbars.ui.screens.overview.ViewScreen
+import com.dknaack.healthbars.ui.screens.overview.OverviewEvent
+import com.dknaack.healthbars.ui.screens.overview.OverviewViewModel
+import com.dknaack.healthbars.ui.screens.overview.OverviewScreen
+import com.dknaack.healthbars.ui.screens.overview.OverviewState
+import com.dknaack.healthbars.ui.screens.upsert.UpsertEvent
 import com.dknaack.healthbars.ui.screens.upsert.UpsertScreen
+import com.dknaack.healthbars.ui.screens.upsert.UpsertViewModel
 import com.dknaack.healthbars.ui.theme.HealthBarsTheme
-import kotlinx.coroutines.launch
 import kotlinx.serialization.Serializable
 
 class MainActivity : ComponentActivity() {
@@ -39,11 +49,36 @@ class MainActivity : ComponentActivity() {
         ).build()
     }
 
-    private val viewModel by viewModels<ListViewModel>(
+    private val listViewModel by viewModels<ListViewModel>(
         factoryProducer = {
             object : ViewModelProvider.Factory {
                 override fun <T : ViewModel> create(modelClass: Class<T>): T {
                     return ListViewModel(db.healthBarDao()) as T
+                }
+            }
+        }
+    )
+
+    private val upsertViewModel by viewModels<UpsertViewModel>(
+        factoryProducer = {
+            object : ViewModelProvider.Factory {
+                override fun <T : ViewModel> create(modelClass: Class<T>): T {
+                    return UpsertViewModel(
+                        db.healthBarDao(),
+                    ) as T
+                }
+            }
+        }
+    )
+
+    private val overviewViewModel by viewModels<OverviewViewModel>(
+        factoryProducer = {
+            object : ViewModelProvider.Factory {
+                override fun <T : ViewModel> create(modelClass: Class<T>): T {
+                    return OverviewViewModel(
+                        healthBarDao = db.healthBarDao(),
+                        logDao = db.logDao(),
+                    ) as T
                 }
             }
         }
@@ -55,45 +90,59 @@ class MainActivity : ComponentActivity() {
 
         setContent {
             HealthBarsTheme {
-                val scope = rememberCoroutineScope()
                 val navController = rememberNavController()
-                val state by viewModel.state.collectAsState()
-                var selectedHealthBar by remember { mutableStateOf<HealthBar?>(null) }
 
-                NavHost(
-                    navController = navController,
-                    startDestination = NavItem.ListScreen,
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
                 ) {
-                    composable<NavItem.ListScreen> {
-                        ListScreen(
-                            state = state,
-                            onEvent = viewModel::onEvent,
-                            navController = navController,
-                        )
-                    }
-                    composable<NavItem.UpsertScreen> {
-                        UpsertScreen(
-                            navController,
-                            onUpsert = {
-                                scope.launch {
-                                    val dao = db.healthBarDao()
-                                    dao.upsert(it)
+                    NavHost(
+                        navController = navController,
+                        startDestination = NavItem.ListScreen,
+                    ) {
+                        composable<NavItem.ListScreen> {
+                            val state by listViewModel.state.collectAsState()
+                            ListScreen(
+                                state = state,
+                                onEvent = listViewModel::onEvent,
+                                navController = navController,
+                            )
+                        }
+                        composable<NavItem.UpsertScreen> {
+                            val args: NavItem.UpsertScreen = it.toRoute()
+                            val state by upsertViewModel.state.collectAsState()
+                            LaunchedEffect(args.id) {
+                                if (args.id != null) {
+                                    upsertViewModel.onEvent(UpsertEvent.SetId(args.id))
+                                } else {
+                                    upsertViewModel.onEvent(UpsertEvent.SetId(0))
                                 }
-                            },
-                            modifier = Modifier.animateEnterExit(
-                                enter = slideInVertically(),
-                                exit = slideOutVertically(),
+                            }
+
+                            UpsertScreen(
+                                state = state,
+                                onEvent = upsertViewModel::onEvent,
+                                navController = navController,
+                                modifier = Modifier.animateEnterExit(
+                                    enter = slideInVertically(),
+                                    exit = slideOutVertically(),
+                                )
                             )
-                        )
-                    }
-                    composable<NavItem.ViewScreen> {
-                        if (selectedHealthBar != null) {
-                            ViewScreen(
-                                navController,
-                                selectedHealthBar!!,
+                        }
+                        composable<NavItem.ViewScreen> {
+                            val args: NavItem.ViewScreen = it.toRoute()
+                            overviewViewModel.onEvent(OverviewEvent.Show(args.id))
+
+                            val state by overviewViewModel.state.collectAsState()
+                            OverviewScreen(
+                                navController = navController,
+                                state = state,
+                                onEvent = overviewViewModel::onEvent,
+                                modifier = Modifier.animateEnterExit(
+                                    enter = slideInVertically(),
+                                    exit = slideOutVertically(),
+                                )
                             )
-                        } else {
-                            navController.popBackStack()
                         }
                     }
                 }
