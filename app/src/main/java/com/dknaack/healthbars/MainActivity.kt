38ff534e312f8eb2841a41d38b4d32fe1a759393
Daniel Knaack <daniel.knaack@protonmail.com>
2025-09-01 12:05:21 +0200

    Optimize imports

--- app/src/main/java/com/dknaack/healthbars/MainActivity.kt
+++ app/src/main/java/com/dknaack/healthbars/MainActivity.kt
@@ -15,10 +15,7 @@ import androidx.compose.material3.Surface
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
