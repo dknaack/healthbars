654599954f70fa04de6610cb2f85b5d403798535
Daniel Knaack <daniel.knaack@protonmail.com>
2025-09-01 12:05:36 +0200

    Add LogDao to AppDatabase

--- app/src/main/java/com/dknaack/healthbars/data/AppDatabase.kt
+++ app/src/main/java/com/dknaack/healthbars/data/AppDatabase.kt
@@ -5,14 +5,29 @@ import androidx.room.RoomDatabase
 import androidx.room.TypeConverter
 import androidx.room.TypeConverters
 import java.time.LocalDate
+import java.time.LocalDateTime
 
-@Database(entities = [HealthBar::class], version = 1)
+@Database(
+    entities = [HealthBar::class, LogEntry::class],
+    version = 2,
+)
 @TypeConverters(Converters::class)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun healthBarDao(): HealthBarDao
+    abstract fun logDao(): LogDao
 }
 
 class Converters {
+    @TypeConverter
+    fun fromLocalDateTime(dateTime: LocalDateTime?): String? {
+        return dateTime?.toString()
+    }
+
+    @TypeConverter
+    fun toLocalDateTime(value: String?): LocalDateTime? {
+        return value?.let { LocalDateTime.parse(it) }
+    }
+
     @TypeConverter
     fun timestampToLocalDate(value: Long?): LocalDate? {
         return value?.let { LocalDate.ofEpochDay(it) }
@@ -21,5 +36,15 @@ class Converters {
     @TypeConverter
     fun localDateToTimestamp(date: LocalDate?): Long? {
         return date?.toEpochDay()
+    }
+
+    @TypeConverter
+    fun fromLogAction(action: LogAction?): String? {
+        return action?.name
+    }
+
+    @TypeConverter
+    fun toLogAction(value: String?): LogAction? {
+        return value?.let { LogAction.valueOf(it) }
     }
 }
